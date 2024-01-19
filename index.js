80855fdf6fc2382e1f0f6ed78e647c2cadbb11fb
Cypher <99555461+Cypher0137@users.noreply.github.com>
2024-01-19 20:06:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.json());
 
 // Fetching questions from the API
 app.get('/api/questions', async (req, res) => {
