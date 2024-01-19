86616bd84e30d67d1192a6e79d3afab314323f5a
Cypher <99555461+Cypher0137@users.noreply.github.com>
2024-01-19 21:23:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const axios = require('axios');
-const path = require('path');
-
-
+const cors = require('cors'); // Import the cors middleware
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 // Fetching questions from the API
@@ -34,56 +33,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const axios = require('axios');
-// const cors = require('cors'); // Import the cors middleware
-// const path = require('path');
-
-// const app = express();
-// const port = process.env.PORT || 3001;
-
-// // Use the cors middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Fetching questions from the API
-// app.get('/api/questions', async (req, res) => {
-//   try {
-//     const response = await axios.get(
-//       'https://opentdb.com/api.php?amount=15'
-//     );
-//     const questions = response.data.results.map((question) => ({
-//       question: question.question,
-//       choices: [...question.incorrect_answers, question.correct_answer],
-//       correctAnswer: question.correct_answer,
-//     }));
-//     res.json(questions);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-// // Handling root route
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the Quiz Application Backend!');
-// });
-
-
-
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-  
